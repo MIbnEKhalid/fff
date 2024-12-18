67aa428ae15cf159f425f942922e1a5133eaa638
Muhammad Bin Khalid <chmuhammadbinkhalid28@gmail.com>
2024-12-18 13:45:51 +0500

    Add notification feature, update static file serving paths, and migrate assets

--- index.js
+++ index.js
@@ -5,18 +5,42 @@ import { dirname } from "path";
 
 const app = express();
 app.use(express.json()); // To parse JSON request bodies
- 
+
+
+// Serve static files
+app.use(
+    "/Assets",
+    express.static(path.join(dirname(fileURLToPath(import.meta.url)), "Assets"), {
+        setHeaders: (res, path) => {
+            if (path.endsWith(".css")) {
+                res.setHeader("Content-Type", "text/css");
+            }
+        },
+    })
+);
+
+app.use(
+    "/AdminPanel",
+    express.static(path.join(dirname(fileURLToPath(import.meta.url)), "AdminPanel"), {
+        setHeaders: (res, path) => {
+            if (path.endsWith(".css")) {
+                res.setHeader("Content-Type", "text/css");
+            }
+        },
+    })
+);
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
  
-app.use("/", express.static(path.join(__dirname, "public/")));
-app.use("/AdminPanel", express.static(path.join(__dirname, "public/AdminPanel")));
-app.use("/AdminPanel", express.static(path.join(__dirname, "public/AdminPanel")));
-app.use("/AdminPanel/Add/Book", express.static(path.join(__dirname, "public/AdminPanel/Add/Book")));
-app.use("/AdminPanel/Add/QuizAss", express.static(path.join(__dirname, "public/AdminPanel/Add/QuizAss")));
-app.use("/history", express.static(path.join(__dirname, "public/history")));
-app.use("/History", express.static(path.join(__dirname, "public/History"))); 
+app.use("/", express.static(path.join(__dirname, "/")));
+app.use("/AdminPanel", express.static(path.join(__dirname, "/AdminPanel")));
+app.use("/AdminPanel", express.static(path.join(__dirname, "/AdminPanel")));
+app.use("/AdminPanel/Add/Book", express.static(path.join(__dirname, "/AdminPanel/Add/Book")));
+app.use("/AdminPanel/Add/QuizAss", express.static(path.join(__dirname, "/AdminPanel/Add/QuizAss")));
+app.use("/history", express.static(path.join(__dirname, "/history")));
+app.use("/History", express.static(path.join(__dirname, "/History"))); 
    
 
 const port =  3000;
